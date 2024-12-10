f90aad16fb22a50099d8128deb6105ae52403b20
Samchenko Alexander <samchenko.aa@yandex.ru>
2024-12-10 18:32:02 +0300

    init new structure

--- src/2Generics/Veil/Veil.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect, useState } from "react";
-import s from "./Veil.module.css";
-
-const Veil = (props) => {
-  return <div className={s.veil}></div>;
-};
-
-export default Veil;
